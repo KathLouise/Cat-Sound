3afb84027f7ed20d1269f593b09b94cceff8236d
KathLouise <kathe.nightwish@gmail.com>
2019-02-17 16:22:31 -0300

    Projeto completo

--- CatSound/ViewController.swift
+++ CatSound/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelMeow: UILabel!
+    @IBOutlet weak var buttonMeow: UIButton!
+    var catSound: AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Para evitar que a imagem sofra deformaçoes dependendo do aparelho que for rodas a aplicação
+        buttonMeow.imageView?.contentMode = .scaleAspectFit
+        labelMeow.isHidden = true
+        
+        //Retorna o caminho onde o arquivo está salvo
+        let soundFile = Bundle.main.path(forResource: "Cat", ofType: "wav")
+        
+        do {
+            try catSound = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFile!))
+        } catch {
+            print(error)
+        }
+        
     }
 
-
+    @IBAction func pressMeowButton(_ sender: Any) {
+        labelMeow.isHidden = false
+        catSound.play()
+        Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(labelHidden), userInfo: nil, repeats: false)
+    }
+    
+    @objc func labelHidden (){
+        labelMeow.isHidden = true
+    }
+    
 }
 
